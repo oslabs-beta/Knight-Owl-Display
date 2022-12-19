45bb4a5b4179d939c672fe91d79e679ca3083e60
Caitlin <cme369@nyu.edu>
2022-12-19 12:08:29 -0500

    NavBar MUI

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+import { AppBar, Button, IconButton, Stack,  Toolbar, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+// import LoginIcon from '@mui/icons-material/Login';
+
 
 const NavBar = () => {
   return (
-    <div className='CE-NavBar' key={uuidv4()}>
-      <nav>
-        <img className='CE-LogoImage' src='../../docs/assets/KnightOwl.PNG'></img>
-        <a>Sign Up</a>
-        <a>About The Team</a>
-        <a>GitHub</a>
-        <a>NPM</a>
-      </nav>
-    </div>
+    <AppBar>
+      <Toolbar>
+        <IconButton>
+          <img className='CE-LogoImage' src='../../docs/assets/KnightOwl.PNG'/>
+        </IconButton>
+        <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
+          KNIGHT OWL
+        </Typography>
+        <Stack direction='row' spacing={2}>
+          <Button color='inherit'>Sign Up</Button>
+          <Button color='inherit'>About The Team</Button>
+          <Button color='inherit'>NPM</Button>
+          <IconButton>
+            <GitHubIcon/>
+          </IconButton>
+        </Stack>
+      </Toolbar>
+    </AppBar>
   )
 }
 
