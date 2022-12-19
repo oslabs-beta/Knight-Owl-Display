ea811224af02cd8dbd69d8dbbc6302ae7f2d7570
Ona Narbutas <ona.narbutas@gmail.com>
2022-12-19 11:49:51 -0500

    Begin creating Auth component

---
+++ client/components/Auth.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const Auth = () => {
+  // use the useState hook to manage state for whether the user is looking at login/auth fields and conditionally render accordingly
+
+  // declare functions to send login/signup info on click to backend for authentication and authorization so user can be
+  // redirected to navbar
+  return (
+    <div className='ON-Auth' key={uuidv4()}>
+    </div>
+  )
+}
+
+export default NavBar;
