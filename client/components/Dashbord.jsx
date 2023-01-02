08732e5855063ea7e2b45a23502f3fe6e6185fbd
Jackson Kalmbach <jacksonrkalmbach@gmail.com>
2023-01-02 16:23:12 -0500

    added query log, reformated dashboard structure

--- client/components/Dashbord.jsx
+++ client/components/Dashbord.jsx
@@ -3,27 +3,44 @@ import { Grid } from "@mui/material";
 import BarChart from './BarChart.jsx';
 import LineChartEx from './LineChartEx.jsx';
 import { LineGraph } from './Graph.jsx';
+import QueryLog from './QueryLog.jsx';
 
 export default function Dashboard() {
   return (
-    <Grid item xs={12} className='full-dash' >
-        <h1>Dashboard</h1>
-        <Grid
+      <Grid
         container
+        spacing={3}
+        className='full-dash'
         direction="row"
+        flexWrap="nowrap"
         justify="center"
-        alignItems="center">
-          <Grid item xs={3}>
-            {/* Insert dashboard component here */}
-            <BarChart />
+        >
+          <Grid item xs={6} >
+            <QueryLog />
           </Grid>
-          <Grid item xs={3}>
-          <LineGraph></LineGraph>
+
+          <Grid item xs={6}>
+            <Grid
+             container
+             maxWidth="lg"
+             className='full-dash'
+             direction="column"
+             flexWrap="nowrap"
+             justify="center"
+            >
+
+              <Grid item xs={12} >
+                <LineChartEx />
+              </Grid>
+              <Grid item xs={12}>
+                {/* Insert dashboard component here */}
+                <BarChart />
+              </Grid>
+              <Grid item xs={12}>
+                <LineGraph></LineGraph>
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-             <LineChartEx />
-          </Grid>
-      </Grid>
       </Grid>
   )
 };
---
+++ client/components/QueryLog.jsx
@@ -0,0 +1,127 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+
+
+const columns = [
+  { id: 'date', label: 'Date', minWidth: 170 },
+  { id: 'querystring', label: 'Query String', minWidth: 100 },
+  {
+    id: 'ip',
+    label: 'IP Address',
+    minWidth: 170,
+    align: 'right',
+  },
+  {
+    id: 'limiter',
+    label: 'Limiter',
+    minWidth: 170,
+    align: 'right',
+  },
+]
+
+function createData(date, querystring, ip, limiter) {
+  
+  return { date, querystring, ip, limiter };
+}
+
+const rows = [
+  createData('1/2/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/13/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/14/2023', '[GraphQL Query]', '135.237.108.247', 'RateLimiter'),
+  createData('1/15/2023', '[GraphQL Query]', '135.237.108.247', 'CostLimiter'),
+  createData('1/16/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/16/2023', '[GraphQL Query]', '133.132.234.525', 'RateLimiter'),
+  createData('1/17/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '182.456.345.123', 'CostLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '125.237.108.247', 'CostLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/13/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/14/2023', '[GraphQL Query]', '135.237.108.247', 'RateLimiter'),
+  createData('1/15/2023', '[GraphQL Query]', '135.237.108.247', 'CostLimiter'),
+  createData('1/16/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/16/2023', '[GraphQL Query]', '133.132.234.525', 'RateLimiter'),
+  createData('1/17/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '182.456.345.123', 'CostLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '135.237.108.247', 'DepthLimiter'),
+  createData('1/4/2023', '[GraphQL Query]', '125.237.108.247', 'CostLimiter'),
+];
+
+export default function QueryLog() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '100%' }} style={{border:"1px solid green", height:'90%'}}>
+      <TableContainer sx={{ maxHeight: "100vh" }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" colSpan={5}>
+                Query Log
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ top: 57, minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
