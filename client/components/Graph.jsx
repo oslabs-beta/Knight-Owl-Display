dd3979ced0dc241e8604cdb76b09c7bf49fc9108
Caitlin <cme369@nyu.edu>
2023-01-02 11:46:22 -0500

    Graph Update

--- client/components/Graph.jsx
+++ client/components/Graph.jsx
@@ -60,14 +60,14 @@ export const data = {
   labels,
   datasets: [
     {
-      label: 'Dataset 1',
+      label: 'Depth Limiter',
       data: [12, 7, 5,76, 4, 55, 7],
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
       yAxisID: 'y',
     },
     {
-      label: 'Dataset 2',
+      label: 'Rate Limiter',
       data: [12, 3427, 53424,743246, 34244, 55432, 7],
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
