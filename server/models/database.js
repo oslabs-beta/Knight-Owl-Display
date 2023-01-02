361fc1e09c2fc58161ce5151b744ccdfaf96fe6a
Ona Narbutas <ona.narbutas@gmail.com>
2023-01-02 12:29:52 -0500

    Connect elephant sql database, add schema to models/models.js as comment, lines 1-25

---
+++ server/models/database.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+const PG_URI = process.env;
+
+const pool = new Pool({
+  connectionString: PG_URI
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query: ', text);
+    return pool.query(text, params, callback);
+  }
+}
--- server/models/models.js
+++ server/models/models.js
@@ -1,3 +1,32 @@
+// current SQL schema:
+
+// CREATE TABLE bad_queries(
+//   query_id SERIAL PRIMARY KEY,
+//   querier_IP_address VARCHAR(45),
+//   query_string VARCHAR(5000),
+//   rejected_on TIMESTAMP,
+//   rejected_by VARCHAR(22),
+//   user_id INTEGER REFERENCES USERS (id)
+//  )
+
+// example query to create new bad_query datapoint: 
+// INSERT INTO bad_queries (querier_IP_Address, query_string, rejected_by, rejected_on, user_id)
+// VALUES ('0:0:0:1', 'Give me money', 'depth_limiter', '2022-12-21 09:35:00', 2)
+
+// CREATE TABLE users (
+//   id  SERIAL PRIMARY KEY,
+//   email  VARCHAR(320),
+//   password  VARCHAR(60),
+//   organization VARCHAR(60)
+// )
+
+// example query to create new user datapoint:
+// INSERT INTO users (email, password, organization)
+// VALUES ('ona@codesmith.com', '$2b$10$kyjppXLINEfIftwJY8dxuO4eoLFZArsbF79uhAf38Mm4Wm7cj.yH2', 'KnightOwl')
+
+
+
+
 // const { DataTypes, Sequelize } = require('sequelize');
 // const { PG_URI } = process.env;
 // const pg = require('pg');
